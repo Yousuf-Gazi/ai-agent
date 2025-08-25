fd185a31ff2f959778b1af7764b648c00bbe775d
Yousuf-Gazi <gaziyousufridoy@gmail.com>
2025-08-25 08:20:50 +0600

    Refactor: Allow prompt to be passed via CLI

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import sys
 import os
 from google import genai
 from dotenv import load_dotenv
@@ -5,15 +6,25 @@ from dotenv import load_dotenv
 
 def main():
     load_dotenv()
+
+    # getting user prompt from the command line arguments
+    args = sys.argv[1:]
+    if not args:
+        print("AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here"')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+    user_prompt = " ".join(args)
+
     api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     response = client.models.generate_content(
         model="gemini-2.0-flash-001",
-        contents="Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+        contents=user_prompt
     )
     print(f"Prompt tokens: {response.usage_metadata.prompt_token_count}")
     print(f"Response tokens: {response.usage_metadata.candidates_token_count}")
-    print("Respons:")
+    print("Response:")
     print(response.text)
 
 
